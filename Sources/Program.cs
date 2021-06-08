e5559fe35c832d21bedff35174a8d083a41f71c8
Chris Simpson <chris.m.simpson@icloud.com>
2021-06-09 09:09:42 +1000

    Fix

---
+++ Sources/Program.cs
@@ -0,0 +1,18 @@
+//
+//
+//
+
+using System;
+
+using static System.Console;
+
+namespace Neu
+{
+    public static partial class Program
+    {
+        public static void Main(String[] args)
+        {
+            WriteLine($"Hello foo");
+        }
+    }
+}
